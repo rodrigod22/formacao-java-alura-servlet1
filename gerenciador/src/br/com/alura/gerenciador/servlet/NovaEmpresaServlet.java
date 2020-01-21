a26cb3c945896280862d814f50776696dbf7122b
Rodrigo Peixoto <rodrigo.peixoto@fatec.sp.gov.br>
2020-01-21 11:03:51 -0300

    parte 3 - definindo o modelo

--- gerenciador/src/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
+++ gerenciador/src/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
@@ -15,11 +15,21 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/novaEmpresa")
 public class NovaEmpresaServlet extends HttpServlet {
 	
-	private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;	
+
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
 		System.out.println("Cadastrando empresa");
 		String nome = request.getParameter("nome");
+		
+		Empresa empresa = new Empresa();
+		empresa.setNome(nome);
+		
+		Banco banco  = new Banco();
+		banco.adiciona(empresa);
+		
+		System.out.println(banco.getEmpresas());
 		
 		PrintWriter out = response.getWriter();
 		out.println("<html>");	
