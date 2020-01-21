24fe0ac8459842664e1a77100cf4ef4498553efe
Rodrigo Peixoto <rodrigo.peixoto@fatec.sp.gov.br>
2020-01-21 16:20:12 -0300

    parte 6 - redirecionando o fluxo

--- gerenciador/src/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
+++ gerenciador/src/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
@@ -41,10 +41,11 @@ public class NovaEmpresaServlet extends HttpServlet {
 		
 		Banco banco  = new Banco();
 		banco.adiciona(empresa);	
-		
-		RequestDispatcher rd = request.getRequestDispatcher("/novaEmpresaCriada.jsp");
-		request.setAttribute("empresa", empresa.getNome());
-		rd.forward(request, response);
+
+		response.sendRedirect("listaEmpresas");
+//		RequestDispatcher rd = request.getRequestDispatcher("/listaEmpresas");
+//		request.setAttribute("empresa", empresa.getNome());
+//		rd.forward(request, response);
 	}
 
 }
