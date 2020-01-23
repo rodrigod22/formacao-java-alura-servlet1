9606c5be0e53253d0f483135a0b006cb2a03563b
Rodrigo Peixoto <rodrigo.peixoto@fatec.sp.gov.br>
2020-01-23 11:11:34 -0300

    modulo 2 parte 3 - implementando login

--- gerenciador/src/br/com/alura/gerenciador/modelo/Banco.java
+++ gerenciador/src/br/com/alura/gerenciador/modelo/Banco.java
@@ -7,6 +7,7 @@ import java.util.List;
 public class Banco {
 
 	private static List<Empresa> lista = new ArrayList<Empresa>();
+	private static List<Usuario> listaUsuarios= new ArrayList<>();
 	private static Integer chaveSequencial = 1;
 	
 	//simulando um banco de dados inicializa com 2 empresas cadastradas
@@ -19,6 +20,17 @@ public class Banco {
 		empresa2.setId(chaveSequencial++);
 		lista.add(empresa);
 		lista.add(empresa2);
+		
+		Usuario u1 = new Usuario();
+	    u1.setLogin("nico");
+	    u1.setSenha("12345");
+
+	    Usuario u2 = new Usuario();
+	    u2.setLogin("ana");
+	    u2.setSenha("12345");
+
+	    listaUsuarios.add(u1);
+	    listaUsuarios.add(u2);
 	}
 	
 	public void adiciona(Empresa empresa) {		
@@ -50,5 +62,15 @@ public class Banco {
 			}
 		}
 		return null;		
+	}
+
+	public Usuario existeUsuario(String login, String senha) {
+		
+		for (Usuario user : listaUsuarios) {
+			if(user.ehIgual(login, senha)) {
+				return user;
+			}
+		}
+		return null;
 	}
 }
